e329dad52e5bf5427e92b665d1ef33779f324974
habibaadam <habiepalmer@gmail.com>
2025-10-17 17:12:41 +0000

    integrating with amazon cognito

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_RqR4nPgek', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '2impj2db6mts632dkm85bk5tqc', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1', // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
-    }
-};
+        invokeUrl: '', // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    },
+}
